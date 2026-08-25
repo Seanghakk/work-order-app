4a701375031517f818d67c06c4f89a75f955b623
Seanghakk <n.seanghakk@gmail.com>
2026-08-25 14:11:00 +0700

    include pdfkit font files in serverless bundle

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      "/api/work-orders/[id]/report": ["./node_modules/pdfkit/js/**/*"],
+    },
+  },
+};
 module.exports = nextConfig;
